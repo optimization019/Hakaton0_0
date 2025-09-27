22eca5060b86c93c33a317b5ab62cae40c06618d
optimization019 <hag1vara228@gmail.com>
2025-09-27 10:07:25 +0500

    NADO

--- app/src/main/java/com/example/hakaton/MainActivity.java
+++ app/src/main/java/com/example/hakaton/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.hakaton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ScrollView;
 
 import androidx.activity.EdgeToEdge;
@@ -21,7 +25,22 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
+        ImageView Test1 = findViewById(R.id.imageView16);
+        Test1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignIn.class);
+                startActivity(intent);
+            }
+        });
+        ImageView Test2 = findViewById(R.id.imageView17);
+        Test2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LogIn.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
